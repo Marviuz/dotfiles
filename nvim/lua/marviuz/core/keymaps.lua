a137d24323729e60029a25b4e2f2cc459f7b5bf5
Marviuz <marviuz.gaming@gmail.com>
2024-04-20 02:58:28 +0800

    added more remaps

--- nvim/lua/marviuz/core/keymaps.lua
+++ nvim/lua/marviuz/core/keymaps.lua
@@ -14,3 +14,6 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+
+keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "vscode alt + down arrow" })
+keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "vscode alt + up arrow" })
