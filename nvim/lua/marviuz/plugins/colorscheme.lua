1a529de4134b223556e980431308a93f018fef80
Marviuz <marviuz.gaming@gmail.com>
2024-04-20 12:23:35 +0800

    changed color scheme

--- nvim/lua/marviuz/plugins/colorscheme.lua
+++ nvim/lua/marviuz/plugins/colorscheme.lua
@@ -1,7 +1,12 @@
 return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme tokyonight")
-  end
+	-- "folke/tokyonight.nvim",
+	-- priority = 1000,
+	-- config = function()
+	--   vim.cmd("colorscheme tokyonight")
+	-- end
+	"EdenEast/nightfox.nvim",
+	priority = 1000,
+	config = function()
+		vim.cmd("colorscheme carbonfox")
+	end,
 }
