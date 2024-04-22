2992894512e754af8e3c2723a16c4e2d85a9cc65
Marviuz <marviuz.gaming@gmail.com>
2024-04-22 20:19:56 +0800

    modified bufferline

--- nvim/lua/marviuz/plugins/bufferline.lua
+++ nvim/lua/marviuz/plugins/bufferline.lua
@@ -1,10 +1,20 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		options = {
+			diagnostics = "nvim_lsp",
+			always_show_bufferline = false,
+			mode = "tabs",
+			separator_style = "slant",
+			offsets = {
+				{
+					filetype = "neo-tree",
+					text = "Neo-tree",
+					highlight = "Directory",
+					text_align = "left",
+				},
+			},
+		},
+	},
 }
