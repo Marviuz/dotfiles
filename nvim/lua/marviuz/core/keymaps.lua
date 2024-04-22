ab26e6d5f0a96078b424942c7de735a378c140d5
Marviuz <marviuz.gaming@gmail.com>
2024-04-22 22:29:44 +0800

    modified keymaps

--- nvim/lua/marviuz/core/keymaps.lua
+++ nvim/lua/marviuz/core/keymaps.lua
@@ -9,13 +9,20 @@ keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+keymap.set("n", "to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keymap.set("n", "tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+
+keymap.set("n", "<C-d>", "<C-d>zz", { desc = "1/2 page down then center view" })
+keymap.set("n", "<C-u>", "<C-u>zz", { desc = "1/2 page up then center view" })
+
+-- Center screen when searching
+keymap.set("n", "n", "nzzzv", { desc = "Center screen when finding" })
+keymap.set("n", "N", "Nzzzv", { desc = "Center screen when finding" })
 
 keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "vscode alt + down arrow" })
 keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "vscode alt + up arrow" })
 
-keymap.set("n", "<leader>ref", vim.lsp.buf.rename, { desc = "Refactor like vscode F2"})
+keymap.set("n", "<leader>ref", vim.lsp.buf.rename, { desc = "Refactor like vscode F2" })
