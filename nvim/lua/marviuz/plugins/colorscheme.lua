7df4200070e9c07c37fb0891f07acae4b0c90151
Marviuz <marviuz.gaming@gmail.com>
2024-04-27 00:17:26 +0800

    used tokyonight night theme

--- nvim/lua/marviuz/plugins/colorscheme.lua
+++ nvim/lua/marviuz/plugins/colorscheme.lua
@@ -1,12 +1,17 @@
 return {
-	-- "folke/tokyonight.nvim",
+	"folke/tokyonight.nvim",
+	priority = 1000,
+	opts = {
+		style = "night",
+	},
+	config = function(_, opts)
+		local tokyonight = require("tokyonight")
+		tokyonight.setup(opts)
+		vim.cmd("colorscheme tokyonight")
+	end,
+	-- "EdenEast/nightfox.nvim",
 	-- priority = 1000,
 	-- config = function()
-	--   vim.cmd("colorscheme tokyonight")
-	-- end
-	"EdenEast/nightfox.nvim",
-	priority = 1000,
-	config = function()
-		vim.cmd("colorscheme carbonfox")
-	end,
+	-- 	vim.cmd("colorscheme carbonfox")
+	-- end,
 }
