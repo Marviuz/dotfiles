7da594fb67690133e531fee86e35fc879fb9f481
Marviuz <marviuz.gaming@gmail.com>
2024-05-01 19:38:58 +0800

    added file browser

--- nvim/lua/marviuz/plugins/telescope.lua
+++ nvim/lua/marviuz/plugins/telescope.lua
@@ -6,10 +6,12 @@ return {
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		"nvim-tree/nvim-web-devicons",
 		"folke/todo-comments.nvim",
+		"nvim-telescope/telescope-file-browser.nvim",
 	},
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
+		local map = require("marviuz.utils.map")
 
 		telescope.setup({
 			pickers = {
@@ -34,18 +36,37 @@ return {
 					},
 				},
 			},
+			extensions = {
+				file_browser = {
+					theme = "ivy",
+					mappings = {
+						["i"] = {
+							-- your custom insert mode mappings
+						},
+						["n"] = {
+							-- your custom normal mode mappings
+						},
+					},
+				},
+			},
 		})
 
 		telescope.load_extension("fzf")
+		telescope.load_extension("file_browser")
 
-		local keymap = vim.keymap
+		map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+		map("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+		map("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+		map("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+		map("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+		map("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 
-		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+		map(
+			"n",
+			"<space>fb",
+			":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+			{ desc = "open file_browser with the path of the current buffer" }
+		)
 	end,
 }
