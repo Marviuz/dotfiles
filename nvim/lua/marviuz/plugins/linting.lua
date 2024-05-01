42c0071ba8e65aa95b0c6594ad54535fbc280ffe
Marviuz <marviuz.gaming@gmail.com>
2024-05-01 19:17:22 +0800

    modified linting

--- nvim/lua/marviuz/plugins/linting.lua
+++ nvim/lua/marviuz/plugins/linting.lua
@@ -3,16 +3,28 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		local lint = require("lint")
+		local map = require("marviuz.utils.map")
 
 		lint.linters_by_ft = {
-			javascript = { "eslint_d" },
-			typescript = { "eslint_d" },
-			javascriptreact = { "eslint_d" },
-			typescriptreact = { "eslint_d" },
-			svelte = { "eslint_d" },
+			typescript = { "eslint_d", "eslint" },
+			javascript = { "eslint_d", "eslint" },
+			typescriptreact = { "eslint_d", "eslint" },
+			javascriptreact = { "eslint_d", "eslint" },
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+		local eslint = lint.linters.eslint_d
+
+		eslint.args = {
+			"--no-warn-ignored", -- <-- this is the key argument
+			"--format",
+			"json",
+			"--stdin",
+			"--stdin-filename",
+			function()
+				return vim.api.nvim_buf_get_name(0)
+			end,
+		}
 
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = lint_augroup,
@@ -21,7 +33,7 @@ return {
 			end,
 		})
 
-		vim.keymap.set("n", "<leader>l", function()
+		map("n", "<leader>l", function()
 			lint.try_lint()
 		end, { desc = "Trigger linting for current file" })
 	end,
