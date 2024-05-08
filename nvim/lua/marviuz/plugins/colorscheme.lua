a54fbcc456dc45877b23119c2cc164cb742eac4f
Marviuz <marviuz.gaming@gmail.com>
2024-05-09 07:24:56 +0800

    modified color scheme

--- nvim/lua/marviuz/plugins/colorscheme.lua
+++ nvim/lua/marviuz/plugins/colorscheme.lua
@@ -23,7 +23,14 @@ return {
 		local catppuccin = require("catppuccin")
 
 		catppuccin.setup({
-			transparent_background = true,
+			-- transparent_background = true,
+			color_overrides = {
+				mocha = {
+					base = "#000000",
+					mantle = "#000000",
+					crust = "#000000",
+				},
+			},
 		})
 
 		vim.cmd.colorscheme("catppuccin-mocha")
