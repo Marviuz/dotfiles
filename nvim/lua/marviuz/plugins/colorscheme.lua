5d23827f825f971a50c9f1f612b42a68ef85d50c
Marviuz <marviuz.gaming@gmail.com>
2024-05-08 20:16:06 +0800

    added catppuccin

--- nvim/lua/marviuz/plugins/colorscheme.lua
+++ nvim/lua/marviuz/plugins/colorscheme.lua
@@ -1,20 +1,34 @@
 return {
-	"folke/tokyonight.nvim",
+	-- "folke/tokyonight.nvim",
+	-- priority = 1000,
+	-- opts = {
+	-- 	style = "night",
+	-- 	transparent = true,
+	-- 	on_highlights = function(hl, c)
+	-- 		hl.TelescopeNormal.bg = "none"
+	-- 		hl.TelescopeBorder.bg = "none"
+	-- 		hl.TelescopePromptBorder.bg = "none"
+	-- 	end,
+	-- },
+	-- config = function(_, opts)
+	-- 	local tokyonight = require("tokyonight")
+	-- 	tokyonight.setup(opts)
+	-- 	vim.cmd("colorscheme tokyonight")
+	-- end,
+
+	"catppuccin/nvim",
+	name = "catppuccin",
 	priority = 1000,
-	opts = {
-		style = "night",
-		transparent = true,
-		on_highlights = function(hl, c)
-			hl.TelescopeNormal.bg = "none"
-			hl.TelescopeBorder.bg = "none"
-			hl.TelescopePromptBorder.bg = "none"
-		end,
-	},
-	config = function(_, opts)
-		local tokyonight = require("tokyonight")
-		tokyonight.setup(opts)
-		vim.cmd("colorscheme tokyonight")
+	config = function()
+		local catppuccin = require("catppuccin")
+
+		catppuccin.setup({
+			transparent_background = true,
+		})
+
+		vim.cmd.colorscheme("catppuccin-mocha")
 	end,
+
 	-- "EdenEast/nightfox.nvim",
 	-- priority = 1000,
 	-- config = function()
