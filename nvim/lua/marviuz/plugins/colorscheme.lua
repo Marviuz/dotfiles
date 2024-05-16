e388f31a66bddbfbb98fd8183a9666ac2fb2d421
Marviuz <marviuz.gaming@gmail.com>
2024-05-16 18:55:44 +0800

    added autoindent and new colorscheme

--- nvim/lua/marviuz/plugins/colorscheme.lua
+++ nvim/lua/marviuz/plugins/colorscheme.lua
@@ -2,43 +2,64 @@ return {
 	-- "folke/tokyonight.nvim",
 	-- priority = 1000,
 	-- opts = {
-	-- 	style = "night",
-	-- 	transparent = true,
-	-- 	on_highlights = function(hl, c)
-	-- 		hl.TelescopeNormal.bg = "none"
-	-- 		hl.TelescopeBorder.bg = "none"
-	-- 		hl.TelescopePromptBorder.bg = "none"
-	-- 	end,
+	--     style = "night",
+	--     transparent = true,
+	--     on_highlights = function(hl, c)
+	--         hl.TelescopeNormal.bg = "none"
+	--         hl.TelescopeBorder.bg = "none"
+	--         hl.TelescopePromptBorder.bg = "none"
+	--     end,
 	-- },
 	-- config = function(_, opts)
-	-- 	local tokyonight = require("tokyonight")
-	-- 	tokyonight.setup(opts)
-	-- 	vim.cmd("colorscheme tokyonight")
+	--     local tokyonight = require("tokyonight")
+	--     tokyonight.setup(opts)
+	--     vim.cmd("colorscheme tokyonight")
 	-- end,
 
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		local catppuccin = require("catppuccin")
-
-		catppuccin.setup({
-			-- transparent_background = true,
-			color_overrides = {
-				mocha = {
-					base = "#000000",
-					mantle = "#000000",
-					crust = "#000000",
-				},
-			},
-		})
-
-		vim.cmd.colorscheme("catppuccin-mocha")
-	end,
+	-- "catppuccin/nvim",
+	-- name = "catppuccin",
+	-- priority = 1000,
+	-- config = function()
+	--     local catppuccin = require("catppuccin")
+	--
+	--     catppuccin.setup({
+	--         -- transparent_background = true,
+	--         color_overrides = {
+	--             mocha = {
+	--                 base = "#000000",
+	--                 mantle = "#000000",
+	--                 crust = "#000000",
+	--             },
+	--         },
+	--     })
+	--
+	--     vim.cmd.colorscheme("catppuccin-mocha")
+	-- end,
 
 	-- "EdenEast/nightfox.nvim",
 	-- priority = 1000,
 	-- config = function()
-	-- 	vim.cmd("colorscheme carbonfox")
+	--     vim.cmd("colorscheme carbonfox")
 	-- end,
+
+	"Mofiqul/vscode.nvim",
+	opts = {
+		style = "dark",
+
+		-- Enable transparent background
+		transparent = true,
+
+		-- Enable italic comment
+		italic_comments = true,
+
+		-- Underline `@markup.link.*` variants
+		underline_links = true,
+
+		-- Disable nvim-tree background color
+		disable_nvimtree_bg = true,
+	},
+	config = function(_, opts)
+		require("vscode").setup(opts)
+		vim.cmd.colorscheme("vscode")
+	end,
 }
