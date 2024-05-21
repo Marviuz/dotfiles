ab45a75e01776146311d77126f74c34b7b6ded3d
Marviuz <marviuz.gaming@gmail.com>
2024-05-21 08:12:49 +0800

    add harpoon marks to tabline

--- nvim/lua/marviuz/plugins/lualine.lua
+++ nvim/lua/marviuz/plugins/lualine.lua
@@ -1,3 +1,23 @@
+local harpoon = require("harpoon")
+
+function harpoon_marks()
+	local contents = {}
+	local marks_length = harpoon:list():length()
+	local current_file_path = vim.fn.fnamemodify(vim.fn.expand("%:p"), ":.")
+	for index = 1, marks_length do
+		local harpoon_file_path = harpoon:list():get(index).value
+		local file_name = harpoon_file_path == "" and "(empty)" or vim.fn.fnamemodify(harpoon_file_path, ":t")
+
+		if current_file_path == harpoon_file_path then
+			contents[index] = string.format("%%#HarpoonNumberActive# %s. %%#HarpoonActive#%s ", index, file_name)
+		else
+			contents[index] = string.format("%%#HarpoonNumberInactive# %s. %%#HarpoonInactive#%s ", index, file_name)
+		end
+	end
+
+	return table.concat(contents)
+end
+
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -6,6 +26,9 @@ return {
 		local lazy_status = require("lazy.status")
 
 		lualine.setup({
+			tabline = {
+				lualine_a = { { harpoon_marks } },
+			},
 			sections = {
 				lualine_x = {
 					{
