22574ec142869a2819fe1e208dde6caade59b917
Marviuz <marviuz.gaming@gmail.com>
2024-06-02 17:02:39 +0800

    modified oil

--- nvim/lua/marviuz/plugins/oil.lua
+++ nvim/lua/marviuz/plugins/oil.lua
@@ -4,6 +4,9 @@ return {
 		view_options = {
 			show_hidden = true,
 		},
+		win_options = {
+			winbar = "%{v:lua.require('oil').get_current_dir()}",
+		},
 	},
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function(_, opts)
