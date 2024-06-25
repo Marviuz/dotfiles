d226db8928ea91f7b0e51382769e427504e776af
Marviuz <marviuz.gaming@gmail.com>
2024-06-26 07:05:24 +0800

    fix undo tree

--- nvim/lua/marviuz/plugins/undotree.lua
+++ nvim/lua/marviuz/plugins/undotree.lua
@@ -3,6 +3,8 @@ return {
 	config = function()
 		local map = require("marviuz.utils.map")
 
+		vim.g.undotree_DiffCommand = "FC"
+
 		map("n", "<leader>ut", vim.cmd.UndotreeToggle, { desc = "Toggle Undotree" })
 	end,
 }
