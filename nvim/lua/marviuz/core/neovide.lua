b2a663d0d931d433c640799effa411ee094d4713
Marviuz <marviuz.gaming@gmail.com>
2024-08-18 21:39:55 +0800

    neovide modification

--- nvim/lua/marviuz/core/neovide.lua
+++ nvim/lua/marviuz/core/neovide.lua
@@ -1,6 +1,7 @@
 if vim.g.neovide then
 	vim.g.neovide_transparency = 0.96
-	vim.o.guifont = "JetBrainsMono Nerd Font Mono:h12"
+	-- vim.o.guifont = "JetBrainsMono Nerd Font Mono:h12"
+	vim.o.guifont = "GeistMono Nerd Font:h12"
 
 	vim.g.neovide_position_animation_length = 0
 	vim.g.neovide_cursor_animation_length = 0.00
