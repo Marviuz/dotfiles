ea1feb3185535d03677d0562683ec8d034cc46c6
Marviuz <marviuz.gaming@gmail.com>
2024-09-09 19:46:13 +0800

    refactor!: tsserver to ts_ls

--- nvim/lua/marviuz/plugins/lsp/lspconfig.lua
+++ nvim/lua/marviuz/plugins/lsp/lspconfig.lua
@@ -100,7 +100,7 @@ return {
 					end,
 				})
 			end,
-			["tsserver"] = function()
+			["ts_ls"] = function()
 				local function organize_imports()
 					local params = {
 						command = "_typescript.organizeImports",
@@ -110,7 +110,7 @@ return {
 					vim.lsp.buf.execute_command(params)
 				end
 
-				lspconfig["tsserver"].setup({
+				lspconfig["ts_ls"].setup({
 					capabilities = capabilities,
 					commands = {
 						OrganizeImports = {
