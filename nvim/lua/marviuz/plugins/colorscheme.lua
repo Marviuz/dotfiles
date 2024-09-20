a4682707c77da8b40fea68fbdf1b3c6b4d6d7824
Marviuz <marviuz.gaming@gmail.com>
2024-09-20 23:26:30 +0800

    transparent colorscheme

--- nvim/lua/marviuz/plugins/colorscheme.lua
+++ nvim/lua/marviuz/plugins/colorscheme.lua
@@ -76,9 +76,7 @@ return {
 	priority = 1000,
 	config = function()
 		require("poimandres").setup({
-			-- leave this setup function empty for default config
-			-- or refer to the configuration section
-			-- for configuration options
+			disable_background = true,
 		})
 
 		vim.cmd.colorscheme("poimandres")
