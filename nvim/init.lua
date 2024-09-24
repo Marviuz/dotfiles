484be05473fad602be95a219edbc8e8aebab2e5f
Marviuz <marviuz.gaming@gmail.com>
2024-09-25 07:28:35 +0800

    reset cursor on vim leave

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,13 @@
-if vim.fn.has("macunix") then
-	vim.api.nvim_exec("language en_US", true)
-end
+vim.api.nvim_exec("language en_US", true)
 
 vim.loader.enable()
 
 require("marviuz.core")
 require("marviuz.lazy")
+
+vim.cmd([[
+    augroup RestoreCursorShapeOnExit
+        autocmd!
+        autocmd VimLeave * set guicursor=a:ver1
+    augroup END
+]])
