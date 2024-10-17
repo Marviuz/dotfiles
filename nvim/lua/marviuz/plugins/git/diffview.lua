d8d3dd454dc1e1be62115f519f71fa6dc4fcfb1f
Marviuz <marviuz.gaming@gmail.com>
2024-10-18 01:25:43 +0800

    readded icons for diffview

--- nvim/lua/marviuz/plugins/git/diffview.lua
+++ nvim/lua/marviuz/plugins/git/diffview.lua
@@ -1,3 +1,8 @@
 return {
 	"sindrets/diffview.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		-- Mini icons currently not supported
+		-- { "echasnovski/mini.icons", version = "*", config = true },
+	},
 }
