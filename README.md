5edc98cdfad95fc90eb7b0628f737b1eb9ff92a1
Marviuz <marviuz.gaming@gmail.com>
2024-10-19 18:18:44 +0800

    updates
    * add wezterm

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # dotfiles
 
-Install this things first
+Install this things first (scoop)
 
-- `gcc`
-- `make`
-- `fzf`
-- `ripgrep`
-- `fd`
-- `nvm`
-  - then install node via `nvm`
+- scoop install
+  - `gcc`
+  - `make`
+  - `fzf`
+  - `ripgrep`
+  - `fd`
+  - `nvm`
+    - then install node via `nvm`
+  - `wezterm-nightly`
+- winget
+  - `winget install GlazeWM`
 
 ## To save space (fr fr it takes up space to my 250gb ssd)
 
@@ -16,6 +20,16 @@ Install this things first
 
 ```
 New-Item -ItemType SymbolicLink -Path "C:\Users\<USER>\AppData\Local\<DIRECTORY>" -Target "E:\dotfiles\<DIRECTORY>"
+```
+
+## Some specific config locations
+
+```sh
+# wezterm
+~/.config/wezterm/wezterm.lua
+
+# Glaze window manager
+~/.glzr
 ```
 
 _Note: Also create symlink for `nvim-data`_
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,78 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+local config = wezterm.config_builder()
+
+config.default_prog = { "pwsh.exe" }
+config.font = wezterm.font("GeistMono Nerd Font")
+config.default_cursor_style = "BlinkingBar"
+config.background = {
+	{
+		source = {
+			File = "D:\\Images\\Wallpapers\\Wuthering Waves\\GZmwQV9WoAghxQ4-transformed-upscaled-edited.png",
+		},
+		horizontal_align = "Center",
+		vertical_align = "Middle",
+		opacity = 0.98,
+		hsb = {
+			brightness = 0.03,
+		},
+	},
+}
+config.color_scheme = "Windows 10 (base16)"
+config.term = "xterm-256color"
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+config.font_size = 12.0
+
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+
+config.keys = {
+	{
+		key = "v",
+		mods = "CTRL|SHIFT|ALT",
+		action = wezterm.action.SplitPane({
+			direction = "Right",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "h",
+		mods = "CTRL|SHIFT|ALT",
+		action = wezterm.action.SplitPane({
+			direction = "Down",
+			size = { Percent = 50 },
+		}),
+	},
+
+	{
+		key = "Y",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		key = "U",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Down", 5 }),
+	},
+	{
+		key = "I",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Up", 5 }),
+	},
+	{
+		key = "O",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Right", 5 }),
+	},
+
+	{ key = "9", mods = "CTRL", action = act.PaneSelect },
+	{ key = "L", mods = "CTRL", action = act.ShowDebugOverlay },
+}
+
+return config
