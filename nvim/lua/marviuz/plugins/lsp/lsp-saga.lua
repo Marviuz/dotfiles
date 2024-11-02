de42aa7e658036f95925526cb0967a8d05bb9879
Marviuz <marviuz.gaming@gmail.com>
2024-11-02 09:29:14 +0800

    modify keymaps

--- nvim/lua/marviuz/plugins/lsp/lsp-saga.lua
+++ nvim/lua/marviuz/plugins/lsp/lsp-saga.lua
@@ -2,7 +2,6 @@ return {
 	"nvimdev/lspsaga.nvim",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
-		-- "nvim-tree/nvim-web-devicons",
 		{ "echasnovski/mini.icons", version = "*" },
 	},
 	config = function()
@@ -21,5 +20,8 @@ return {
 		map("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", { desc = "Refactor variables like VSCode's F2" })
 		map("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", { desc = "Code actions" })
 		map("n", "<leader>k", "<cmd>Lspsaga show_line_diagnostics<CR>", { desc = "Show line diagnostics" })
+		map("n", "K", "<cmd>Lspsaga hover_doc<CR>", { desc = "Show hover doc" })
+		map("n", "<leader>pd", "<cmd>Lspsaga peek_definition<CR>", { desc = "Peek definition" })
+		map("n", "<leader>ptd", "<cmd>Lspsaga peek_type_definition<CR>", { desc = "Peek type definition" })
 	end,
 }
--- nvim/lua/marviuz/plugins/lsp/lspconfig.lua
+++ nvim/lua/marviuz/plugins/lsp/lspconfig.lua
@@ -63,8 +63,9 @@ return {
 				opts.desc = "Go to next diagnostic"
 				map("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
-				opts.desc = "Show documentation for what is under cursor"
-				map("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+				-- Use Lspsaga to hover
+				-- opts.desc = "Show documentation for what is under cursor"
+				-- map("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
 				opts.desc = "Restart LSP"
 				map("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
