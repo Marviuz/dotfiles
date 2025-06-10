c708dfc4ca2ba23527f612d6a9c92177b499c7ac
Marviuz <marviuz.dev@gmail.com>
2025-06-10 08:30:09 +0800

    cleanup

--- waybar/.config/waybar/hyprland_opacity_preview.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-opacityValue=$(hyprctl getoption decoration:active_opacity | grep -oP '(?<=float: )\d+\.\d+')
-
-if (( $(echo "$opacityValue < 1"| bc -l) )); then 
-    echo ""
-else
-    echo ""
-fi
