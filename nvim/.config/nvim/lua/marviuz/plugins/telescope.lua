7a308bbe4c4309e3143aace90ae68c8a3a8ca7a2
Marviuz <marviuz.dev@gmail.com>
2025-06-10 15:14:27 +0800

    modify keybinds

--- nvim/.config/nvim/lua/marviuz/plugins/telescope.lua
+++ nvim/.config/nvim/lua/marviuz/plugins/telescope.lua
@@ -19,11 +19,6 @@ return {
 		local telescope_util = require("marviuz.utils.telescope")
 		local Snacks = require("snacks")
 
-		-- Alternative to Telescope because some files opened by telescope are false mistake
-		-- map("n", "<leader>fmp", function()
-		-- 	Snacks.picker.files()
-		-- end, { desc = "Open fines using mini.pick" })
-
 		telescope.setup({
 			pickers = {
 				find_files = telescope_util.select_find_command(),
@@ -63,19 +58,36 @@ return {
 		telescope.load_extension("oil")
 
 		-- map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-
 		-- Experimental - looks better with double brackets
 		map("n", "<leader>ff", function()
 			Snacks.picker.files({
+				win = {
+					input = {
+						keys = {
+							["<c-d>"] = { "preview_scroll_down", mode = { "i", "n" } },
+							["<c-u>"] = { "preview_scroll_up", mode = { "i", "n" } },
+						},
+					},
+				},
 				hidden = true,
 				ignored = true,
 				exclude = telescope_util.excludes,
 				auto_close = false,
 			})
-		end, { desc = "Open fines using mini.pick" })
+		end, { desc = "Open files using mini.pick" })
 
 		map("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-		map("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+
+		-- map("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+		map("n", "<leader>fs", function()
+			Snacks.picker.grep({
+				hidden = true,
+				ignored = true,
+				exclude = telescope_util.excludes,
+				auto_close = false,
+			})
+		end, { desc = "Grep using mini.pick" })
+
 		map("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
 		map("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 		map("n", "<leader>fg", "<cmd>Telescope git_status<cr>", { desc = "View modified files" })
