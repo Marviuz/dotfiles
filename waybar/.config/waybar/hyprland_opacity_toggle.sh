1c222e83ccb803186dda3fb59fdfc9ac82580e0a
Marviuz <marviuz.dev@gmail.com>
2025-06-10 11:36:45 +0800

    updates

--- waybar/.config/waybar/hyprland_opacity_toggle.sh
+++ waybar/.config/waybar/hyprland_opacity_toggle.sh
@@ -3,13 +3,11 @@
 opacityValue=$(hyprctl getoption decoration:active_opacity | grep -oP '(?<=float: )\d+\.\d+')
     
 if (( $(echo "$opacityValue < 1"| bc -l) )); then 
-    # hyprctl --batch "keyword decoration:active_opacity 1; keyword decoration:inactive_opacity 1"
     active=1
     inactive=1
 else
-    # hyprctl --batch "keyword decoration:active_opacity 0.9; keyword decoration:inactive_opacity 0.8" # Reference the hyprland config for the default values
-    active=0.9
-    inactive=0.8
+    active=0.8
+    inactive=0.7
 fi
 
 hyprctl --batch "keyword decoration:active_opacity $active; keyword decoration:inactive_opacity $inactive"
