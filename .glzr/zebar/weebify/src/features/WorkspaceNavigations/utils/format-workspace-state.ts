2f130d5bed8f699df16e4bbf62e5c52667aabef4
Marviuz <marviuz.dev@gmail.com>
2025-09-05 13:20:40 +0800

    remove glzr

--- .glzr/zebar/weebify/src/features/WorkspaceNavigations/utils/format-workspace-state.ts
+++
@@ -1,5 +0,0 @@
-type FormatWorkspaceStateArguments = ["focused" | null, "displayed" | null];
-
-export function formatWorkspaceState(...args: FormatWorkspaceStateArguments) {
-  return args.filter(Boolean).join("-");
-}
